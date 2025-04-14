dd87a1bb45f7106494e177a9f5754d31b58b17fd
z-vanadium <105497687@qq.com>
2025-04-14 19:12:59 +0800

    russia city faith

--- civ/Russia.sql
+++ civ/Russia.sql
@@ -1,12 +1,15 @@
--- get 1 faith for city center
+-- get 2 faith for city center
 INSERT INTO TraitModifiers(TraitType , ModifierId) VALUES
-    ('TRAIT_CIVILIZATION_MOTHER_RUSSIA', 'BBGV_ONE_FAITH_CITY_CENTER');
+    ('TRAIT_CIVILIZATION_MOTHER_RUSSIA', 'BBGV_CITY_CENTER_EXTRA_FAITH');
 INSERT INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId) VALUES
-    ('BBGV_ONE_FAITH_CITY_CENTER', 'MODIFIER_PLAYER_ADJUST_PLOT_YIELD', 'BBGV_CITY_CENTER_REQSET');
+    ('BBGV_CITY_CENTER_EXTRA_FAITH', 'MODIFIER_PLAYER_ADJUST_PLOT_YIELD', 'BBGV_CITY_CENTER_REQSET');
 INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
-    ('BBG_ONE_FAITH_CAPITAL', 'YieldType', 'YIELD_FAITH'),
-    ('BBG_ONE_FAITH_CAPITAL', 'Amount', 2);
+    ('BBGV_CITY_CENTER_EXTRA_FAITH', 'YieldType', 'YIELD_FAITH'),
+    ('BBGV_CITY_CENTER_EXTRA_FAITH', 'Amount', 2);
 
 -- req
-
+INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES
+    ('BBGV_CITY_CENTER_REQSET', 'REQUIREMENTSET_TEST_ALL');
+INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES
+    ('BBGV_CITY_CENTER_REQSET', 'BBG_REQUIRES_PLOT_IS_CITY_CENTER');
     
