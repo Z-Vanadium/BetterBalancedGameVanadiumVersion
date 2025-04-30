d73159198374841326802fc9a0a445779013e2b1
z-vanadium <105497687@qq.com>
2025-04-30 22:25:19 +0800

    arabia combat back to +5

--- civ/Arabia.sql
+++ civ/Arabia.sql
@@ -1,5 +1,5 @@
 -- combat bonus to +3
-UPDATE ModifierArguments SET Value = 3 WHERE ModifierId = 'BBG_SULTAN_COMBAT_ADJACENT_APOSTLE_MODIFIER' AND Name = 'Amount';
+-- UPDATE ModifierArguments SET Value = 3 WHERE ModifierId = 'BBG_SULTAN_COMBAT_ADJACENT_APOSTLE_MODIFIER' AND Name = 'Amount';
 
 -- remove discount of campus and emcampmemt
 UPDATE ModifierArguments SET Value = 0 WHERE ModifierId LIKE 'BBG_SULTAN_CHEAPER_PURCHASE%' AND Name = 'Amount';
